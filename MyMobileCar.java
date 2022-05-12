446c368da2216b727298b492e37d0de847910056
pavithrasharmila <105265744+pavithrasharmila@users.noreply.github.com>
2022-05-12 10:41:24 +0530

    Add files via upload

---
+++ MyMobileCar.java
@@ -0,0 +1,15 @@
+package java1;
+
+public class MyMobileCar {
+
+
+		public static void main(String[] args) {
+			MobileClass mymob=new MobileClass();
+			mymob.sendMessage();
+			mymob.shareDocument();
+			mymob.call();
+			// TODO Auto-generated method stub
+
+		}
+
+	}
